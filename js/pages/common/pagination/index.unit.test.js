f835431be0966338d2cefd522da82a152af76d13
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-19 14:02:05 +0200

    feat :pagination unit test with 0 in params

--- js/pages/common/pagination/index.unit.test.js
+++ js/pages/common/pagination/index.unit.test.js
@@ -4,4 +4,8 @@ describe("Pagination Unit test suites", () => {
   it("should return something", () => {
     expect(Pagination.getNumberOfPages(12)).toBeDefined();
   });
+
+  it(`should return : "0"`, () => {
+    expect(Pagination.getNumberOfPages(0)).toBe(0);
+  });
 });
