4468ccc51881ff5d7247231ace2bc2e0960ad2e6
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-26 00:04:44 +0200

    feat : unit test should return something

--- js/samples/unit/toBeDefine.test.js
+++ js/samples/unit/toBeDefine.test.js
@@ -1,3 +1,7 @@
 const userName = () => "vikito";
 
 const userAge = 18;
+
+describe("toBeDefined unit test suites", () => {
+  it("should return something", () => expect(userName()).toBeDefined());
+});
