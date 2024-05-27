b9a73ba21a51dc97e1b03f993c97e2c30ce64e8f
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-27 23:26:08 +0200

    feat : unit test should return NaN

--- js/samples/unit/toBeNan.test.js
+++ js/samples/unit/toBeNan.test.js
@@ -5,4 +5,7 @@ describe("toBeNan unit test suites", () => {
   it("souldn't return a Nan", () => {
     expect(sum(2, 8)).not.toBeNaN();
   });
+  it("sould return a Nan", () => {
+    expect(string(2, 8)).toBeNaN();
+  });
 });
