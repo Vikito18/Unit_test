660dca765965a929f364fd46cb8bc202fd14fc7b
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-27 23:25:15 +0200

    feat : unit test shouldn't also return NaN

--- js/samples/unit/toBeNan.test.js
+++ js/samples/unit/toBeNan.test.js
@@ -1,2 +1,8 @@
 const sum = (a, b) => a + b;
 const string = (a, b) => a * b;
+
+describe("toBeNan unit test suites", () => {
+  it("souldn't return a Nan", () => {
+    expect(sum(2, 8)).not.toBeNaN();
+  });
+});
