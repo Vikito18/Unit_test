b40c3afa22eceb9b8c632e87d44c031d16c0ad39
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-29 23:02:49 +0200

    feat : unit test should contain serge

--- js/samples/unit/toContain.test.js
+++ js/samples/unit/toContain.test.js
@@ -1,1 +1,7 @@
 const nameArray = ["Gregoin", "Serge", "Mr Bean"];
+
+describe("toContain unit test suite", () => {
+  it("should contain the name Serge", () => {
+    expect(nameArray).toContain("Serge");
+  });
+});
