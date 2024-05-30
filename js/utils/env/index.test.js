07deff8ad9d3007162874c8ffb9dac0d4b3925b8
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-05-30 18:08:20 +0200

    feat : unit test isInTestEnv shoiuld return false

--- js/utils/env/index.test.js
+++ js/utils/env/index.test.js
@@ -1,7 +1,13 @@
 import { isInTestEnv } from ".";
 
 describe("isInTestEnv unit test suites", () => {
-  it("should return true", () => {
+  it("should return true test env", () => {
     expect(isInTestEnv()).toBe(true);
+  });
+
+  it("should return false not test env", () => {
+    process.env.NODE_ENV = "hey";
+
+    expect(isInTestEnv()).toBe(false);
   });
 });
