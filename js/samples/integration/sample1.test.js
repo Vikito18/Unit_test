84a95113bc6327061421b7c5548f26884c9c0be1
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-06-01 20:29:24 +0200

    feat :sample1 integration test

--- js/samples/integration/sample1.test.js
+++ js/samples/integration/sample1.test.js
@@ -1,1 +1,15 @@
 import { getByTestId } from "@testing-library/dom";
+
+describe("Sample 1 integration test suites", () => {
+  it("should display 'Hello Victor'", () => {
+    const wrapper = document.createElement("div");
+
+    wrapper.innerHTML = `
+    <div id="root">
+    <h1 data-testid="hello">Hello Victor</h1>
+    </div>
+    `;
+
+    expect(getByTestId(wrapper, "hello").textContent).toEqual("Hello Victor");
+  });
+});
