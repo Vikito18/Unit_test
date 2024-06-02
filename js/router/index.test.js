a70128fd93478fbba308d9b0262a9fc296d33728
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2024-06-02 17:49:29 +0200

    feat : router test for signIn page

--- js/router/index.test.js
+++ js/router/index.test.js
@@ -1,3 +1,20 @@
 /**
  * @jest-environment jsdom
  */
+
+import "@testing-library/jest-dom";
+import { getByTestId } from "@testing-library/dom";
+import { router } from ".";
+
+describe("route integration test suites", () => {
+  it("should display signIn page ", async () => {
+    document.body.innerHTML = `
+    <div id="root"></div>`;
+
+    await router();
+
+    expect(getByTestId(document.body, "sign-in-form-title")).toHaveTextContent(
+      "Veuillez vous connecter"
+    );
+  });
+});
